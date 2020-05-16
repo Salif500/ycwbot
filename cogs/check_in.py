c996c77dd30e8d65ebd75895f79829e0d39d55ff
Salif500 <moddedquest@gmail.com>
2020-05-16 12:05:44 -0700

    hi

--- cogs/check_in.py
+++ cogs/check_in.py
@@ -2,50 +2,65 @@ import discord
 from discord.ext import commands
 import json
 
-
+total = 0
 class Check_In(commands.Cog):
 
     def __init__(self, client):
         """:this is the init function for the class"""
         self.client = client
-        total = 0
-        
-        
-
+    
     @commands.command(aliases=["check", "Admin", "c", "checkin"])
+    @commands.has_role("Student")
     async def check_in(self, ctx):
         """:this function checks in the user, while adding information to the database"""
-        user_id = ctx.message.author.id
-        with open("check_in.txt") as file_object:
-            array = file_object.readlines()
-            await ctx.send(str(len(array)))
-            user_id2 = array[0]
-            group2 = array[1]
-            if(user_id == user_id2):
-                await ctx.send("Hello! Your Group is {}.".format(group2))
-                total += 1
+        global total
+        user = ctx.author
+        list1 = str(user).split("#")
+        name = list1[0]
+        print(name)
+        for role in user.roles:
+            print(role)
+            if(str(role) == "Python G1"):
+                print("It works!")
+                await ctx.send("Hello {}. Please Go To Advanced Group".format(name))
+            elif(str(role) == "Python G2"):
+                await ctx.send("Hello {}. Please Go To Intermediate Group".format(name))
+            elif(str(role) == "Python G3"):
+                await ctx.send("Hello {}. Please Go To Beginner Group".format(name))
+            elif(str(role) == "HTML G1"):
+                await ctx.send("Hello {}. Please Go To Beginner Group".format(name))
+            elif(str(role) == "HTML G2"):
+                await ctx.send("Hello {}. Please Go To Beginner Group".format(name))
+        total += 1
+                
 
-    @commands.command(aliases=["workshop_end", "end_workshop"])
-    @commands.has_role("Admin")
-    async def end(self, date):
+    @commands.command(aliases=["clear_cache", "final"])
+    @commands.has_any_role("Admin", "Moderator")
+    async def clear_members(self, ctx, *, date):
+        global total
         """:This function adds the total members joined to the database and clears the current variable"""
-        with open(filename2, "a") as file_object:
-            file_object.write("{}:{} members joined\n".format(date, total))
-        total = 0
+        if(date == ""):
+            await ctx.send("You didnt enter a date")
+        elif(date != ""):
+            with open("total.txt", "a") as file_object:
+                file_object.write("{}: {} members joined\n".format(date, total))
+            total = 0
+            await ctx.send("Data sent to Database")
 
     @commands.command(aliases=["find_data", "info"])
-    @commands.has_role("Admin")
+    @commands.has_any_role("Admin", "Moderator")
     #This checks if the user has the permission of Administrator
     async def data(self, ctx):
+        global total
         """:this tells the user the members joined each week"""
         with open("total.txt") as file_object:
-            for line in file_object:
-                await ctx.send(file_object.readline())
+            lines = file_object.readlines()
+            for line in lines:
+                try:
+                    await ctx.send("\n" + line)
+                except:
+                    print("Empty message")
                 
-    @commands.command()
-    async def test(self, ctx):
-      await ctx.send("{}".format(ctx.message.author.id))
-
 def setup(client):
     client.add_cog(Check_In(client))
                 
--- ycw.py
+++ ycw.py
@@ -6,18 +6,34 @@ client = commands.Bot(command_prefix = ".")
 
 @client.event
 async def on_ready():
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game("Some Codin"))    
     print("Bot is ready")
 
 
 @client.command()
-@commands.has_role("Admin")
+@commands.has_any_role("Admin", "Moderator")
 async def load(ctx, extension):
     client.load_extension("cogs.{}".format(extension))
 
 @client.command()
-@commands.has_role("Admin")
+@commands.has_any_role("Admin", "Moderator")
 async def unload(ctx, extension):
     client.unload_extension("cogs.{}".format(extension))
+
+@client.command(aliases=["move_to_end", "end", "workshop_end", "e"])
+@commands.has_any_role("Admin", "Moderator")
+async def move_end(ctx, *, channel: discord.VoiceChannel):
+    print(channel)
+    for guild in client.guilds:
+        for member in guild.members:
+            if(member.status != discord.Status.offline):
+                try:
+                    await member.move_to(channel)
+                except:
+                    print("{} is not connected\n".format(member))
+
+            
+        
 
 
 
